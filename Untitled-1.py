d1b351b4299d1f9cffcfbbb557794f5ccac47f66
jenn29300 <jenn29300@gmail.com>
2023-11-20 20:32:20 -0600

    primerrepo

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+def sum_list(lista):
+    return sum (lista)
+
+lista_de_elementos =[ 1, 23, 45, 65,  ]
+suma_total= sum_list (lista_de_elementos)
+print ("La suma total de los elementos es:", suma_total)
